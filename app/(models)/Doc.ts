caec512206d75566f268bbcf5e5a8d01e2b04775
apangnathip <alanpangnathip@gmail.com>
2023-11-06 15:09:01 -0500

    rename schema to doc for consistency

---
+++ app/(models)/Doc.ts
@@ -0,0 +1,23 @@
+import mongoose, { InferSchemaType, Schema } from "mongoose";
+
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+} else {
+  throw new Error("DB connection error");
+}
+
+mongoose.Promise = global.Promise;
+
+const docSchema = new Schema(
+  {
+    title: String,
+  },
+  {
+    timestamps: true,
+  },
+);
+
+type DocType = InferSchemaType<typeof docSchema>;
+
+const Doc = mongoose.models.Doc || mongoose.model<DocType>("Doc", docSchema);
+export default Doc;
