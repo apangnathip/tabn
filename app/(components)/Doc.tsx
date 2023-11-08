ae9329a89a4c9a26f554fa5651d0a72073f3d810
apangnathip <alanpangnathip@gmail.com>
2023-11-07 20:21:34 -0500

    remove letter aspect ratio in favour of hard coded values

--- app/(components)/Doc.tsx
+++ app/(components)/Doc.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Doc = () => {
   return (
     <div className="w-48">
-      <div className="letter w-48 bg-gray-500"></div>
+      <div className="w-48 bg-gray-500"></div>
       <div className="bg-gray-300 p-4">
         <p className="text-base font-medium">Something</p>
         <p className="text-sm font-light">blahblublah</p>
