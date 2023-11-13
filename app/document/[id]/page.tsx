b762385141329556f960f6e4fefcbb4d89954033
apangnathip <alanpangnathip@gmail.com>
2023-11-13 11:41:50 -0500

    add container to document page for data fetching

--- app/document/[id]/page.tsx
+++ app/document/[id]/page.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import Document from "./Document";
 
-const document = ({ params }: { params: { id: string } }) => {
-  return <div>{params.id}</div>;
+const getDocById = async (id: string) => {
+  const res = await fetch(`http://localhost:3000/api/Docs/${id}`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to get document");
+  }
+
+  return res.json();
 };
 
-export default document;
+const DocumentContainer = async ({ params }: { params: { id: string } }) => {
+  const doc = await getDocById(params.id);
+
+  return (
+    <div>
+      <Document doc={doc} />
+    </div>
+  );
+};
+
+export default DocumentContainer;
